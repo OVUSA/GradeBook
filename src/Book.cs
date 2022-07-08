a496d86717da928bb6db51e9791a6b4e2bcd289b
Olga Velichko <Ovelichko@Paylocity.com>
2022-07-07 19:52:43 -0500

    reorganized files

--- src/Book.cs
+++ src/Book.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 //is folder name
 namespace GradeBook{
 
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+
 namespace GradeBook{
     class Program{
         static void Main(string [] args){
